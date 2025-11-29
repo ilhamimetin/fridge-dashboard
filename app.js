4cb829ba0a0c3badb209a48c9119ed5936ebe1f9
ilhamimetin <109869045+ilhamimetin@users.noreply.github.com>
2025-11-30 00:37:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// TEMİZLİK: ESKİ TIMESTAMP'LERİ SİL (BİR KERE ÇALIŞTIR)
-firebase.database().ref("devices/kitchen/lastUpdate").remove();
-firebase.database().ref("devices/kitchen/fridgeLastUpdate").remove();
-firebase.database().ref("devices/kitchen/freezerLastUpdate").remove();
-console.log("🧹 Eski timestamp'ler temizlendi");
-
 // Global Variables
 let lastFridgeUpdate = null;
 let lastFreezerUpdate = null;
